b2ca65378c075ed533be8e4ba5a91ee1e5e8e4cc
Kanokphit <kanokphit55@gmail.com>
2024-06-06 15:05:34 +0700

    "fix:edit file name"

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import PostData from "../data/post";
+import PostData from "../data/postdata";
 
 function Posts() {
   const [postDataState, setPostDataState] = useState(PostData);
