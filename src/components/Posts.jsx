dbd10cb4a705def1adb9959683199d0aa6484720
Kanokphit <kanokphit55@gmail.com>
2024-06-06 15:01:33 +0700

    "React Manipulating Array State Posts Assignment"

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,28 +1,66 @@
+import { useState } from "react";
+import PostData from "../data/post";
+
 function Posts() {
+  const [postDataState, setPostDataState] = useState(PostData);
+
+  // const plusLike = (index) => {
+  //   let newPost = [...postDataState];
+  //   newPost[index].likes = newPost[index].likes + 1;
+  //   setPostDataState(newPost);
+  // };
+  // const minusLike = (index) => {
+  //   let newPost = [...postDataState];
+  //   newPost[index].likes = newPost[index].likes - 1;
+  //   setPostDataState(newPost);
+  // };
+
+  const calculateLike = (text, index) => {
+    let newPost = [...postDataState];
+    if (text === "like") {
+      newPost[index].likes = newPost[index].likes + 1;
+    } else if (text === "dislike") {
+      if (newPost[index].likes === 0) {
+        return;
+      }
+      newPost[index].likes = newPost[index].likes - 1;
+    }
+    setPostDataState(newPost);
+  };
   return (
     <div class="app-wrapper">
       <h1 class="app-title">Posts</h1>
       <div class="post-list">
-        <div class="post-item">
-          <div class="post-header">
-            <h2>Post Title #1</h2>
-            <div class="post-social-media-stats">
-              <span class="stats-topic">Likes: </span>
-              <span class="post-likes">10</span>
+        {postDataState.map((item, index) => (
+          <div class="post-item" key={index}>
+            <div class="post-header">
+              <h2>{item.title}</h2>
+              <div class="post-social-media-stats">
+                <span class="stats-topic">Likes: </span>
+                <span class="post-likes">{item.likes}</span>
+              </div>
+            </div>
+            <p class="post-content">{item.content}</p>
+            <div class="post-actions">
+              <button
+                class="like-button"
+                onClick={() => {
+                  calculateLike("like", index);
+                }}
+              >
+                Like
+              </button>
+              <button
+                class="dislike-button"
+                onClick={() => {
+                  calculateLike("dislike", index);
+                }}
+              >
+                Dislike
+              </button>
             </div>
           </div>
-          <p class="post-content">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-            vel turpis vestibulum, aliquet ipsum vitae, auctor risus. Morbi
-            tincidunt, leo non molestie consectetur, elit libero faucibus
-            tellus, sed fringilla tortor libero sit amet odio. Maecenas sed ante
-            condimentum mauris euismod pellentesque eu eu justo...
-          </p>
-          <div class="post-actions">
-            <button class="like-button">Like</button>
-            <button class="dislike-button">Dislike</button>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
